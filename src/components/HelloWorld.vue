2a2eb26927d2eaa635d17a0c5c20f8e50bd9ff35
MichalZiolkiewicz <m.ziolkiewicz1@gmail.com>
2021-06-22 01:00:17 +0200

    Testowanko komponentów

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,11 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <p v-if="counter >= 3 && counter <= 9">Hejka {{ name }}</p>
+    <p> Punkty: {{ counter }} </p>
+    <button v-on:click="addToCounter">+1</button>
+    <button v-if="counter > 0" v-on:click="counter--">-1</button>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-bind:key="user.id" v-for="user in users">User {{ user.name }}, at age {{ user.age }} </li>
     </ul>
   </div>
 </template>
@@ -34,7 +14,23 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+  },
+  data: function () {
+    return {
+      name: 'Michał',
+      counter: 0,
+      users: [
+        {name: 'Michał', age: 24, id: 1},
+        {name: 'Julka', age: 23, id: 2},
+        {name: 'Tomek', age: 20, id: 3}
+      ]
+    }
+  },
+  methods: {
+    addToCounter() {
+      this.counter++
+    }
   }
 }
 </script>
